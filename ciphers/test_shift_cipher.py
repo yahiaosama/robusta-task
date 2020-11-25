a4cb02dfa7bb1941e4a4e1961e846f402eec5398
yahia <yahia.osama8@gmail.com>
2020-11-25 11:56:59 +0200

    Added units tests for shift and matrix algorithms

---
+++ ciphers/test_shift_cipher.py
@@ -0,0 +1,25 @@
+import unittest
+from shift_cipher import ShiftCipher
+from unknowntype_error import UnknownTypeError
+
+class TestShiftCipher(unittest.TestCase):
+    def setUp(self):
+        self.shift_cipher = ShiftCipher("Hello World")
+
+    def test_constructor(self):
+        with self.assertRaises(UnknownTypeError):
+            ShiftCipher(3)
+
+    def test_encrypt(self):
+        self.assertEqual(self.shift_cipher.encrypt(), "Khoor Zruog")
+        self.shift_cipher.given_string = "KOKO"
+        self.assertEqual(self.shift_cipher.encrypt(), "NRNR")
+
+    def test_decrypt(self):
+        self.shift_cipher.given_string = "Khoor Zruog"
+        self.assertEqual(self.shift_cipher.decrypt(), "Hello World")
+        self.shift_cipher.given_string = "NRNR"
+        self.assertEqual(self.shift_cipher.decrypt(), "KOKO")
+
+if __name__ == '__main__':
+    unittest.main()
